e4c71bc02a6f6a6cd15366f18467433ef2b1c23e
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-16 23:01:49 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { mermaidAPI } = require('mermaid');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFileAsync = promisify(fs.writeFile);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Handle POST requests to /png endpoint
+app.post('/png', async (req, res) => {
+  try {
+    const { code, options } = req.body;
+
+    // Generate the PNG using mermaid API with specified options
+    const pngBuffer = mermaidAPI.render(code, options);
+
+    // Send the generated PNG as a response
+    res.set('Content-Type', 'image/png');
+    res.send(pngBuffer);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Handle POST requests to /svg endpoint
+app.post('/svg', async (req, res) => {
+  try {
+    const { code, options } = req.body;
+
+    // Generate the SVG using mermaid API with specified options
+    const svgCode = mermaidAPI.render(code, options);
+
+    // Send the generated SVG as a response
+    res.set('Content-Type', 'image/svg+xml');
+    res.send(svgCode);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
