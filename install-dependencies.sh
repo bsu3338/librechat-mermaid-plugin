33a8784d92c5ec2f1d8506457a44368abe03c130
bsu3338 <bsu3338@users.noreply.github.com>
2023-07-18 07:53:55 -0500

    Create install-dependencies.sh

---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install chromium and required fonts
+# available fonts https://wiki.alpinelinux.org/wiki/Fonts
+apk add chromium font-noto-cjk font-noto-emoji \
+    terminus-font ttf-dejavu ttf-freefont ttf-font-awesome \
+    ttf-inconsolata ttf-linux-libertine \
+    && fc-cache -f
